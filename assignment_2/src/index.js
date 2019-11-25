83a709bfce4a2bc426dcdc8aee276b04660d50c1
joann <joann@qburst.com>
2019-11-25 14:46:59 +0530

    Assignment 2 updates

--- assignment_2/src/index.js
+++ assignment_2/src/index.js
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-/* import App from './App';
-import './index.css'; */
+import App from './App';
+import './index.css';
 
 
 
@@ -22,5 +22,6 @@ return(
 }
 
 
-/* ReactDOM.render(<App />, document.getElementById('root')); */
-ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+/*
+ReactDOM.render(<Test />, document.getElementById('root')); */
